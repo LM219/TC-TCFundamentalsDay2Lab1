614cb3421aa0c95b73cd7204cc527f4b49c0fd78
Louise Milligan <louise.milligan@hotmail.co.uk>
2017-08-10 17:22:43 +0100

    Finished Lab

--- src/main/java/io/zipcoder/fundamentals/day2/lab1/Greeting.java
+++ src/main/java/io/zipcoder/fundamentals/day2/lab1/Greeting.java
@@ -1,9 +1,24 @@
 package io.zipcoder.fundamentals.day2.lab1;
 
+import java.util.Scanner;
+
 
 public class Greeting {
 
     public static void main(String[] args){
-
+    	
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println("What is your name?");
+    
+    String name = scanner.nextLine();
+    
+    if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")){
+    	System.out.format("Hi %s, whaaaaatttsssup?!", name);
+    }
+    	else {
+    		System.out.println("Sorry I do not know you....Goodbye");
+    	}
     }
 }
+
